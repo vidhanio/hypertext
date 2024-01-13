6f75d784014e194db32e4172ae146d1a21fb9377
Vic Demuzere <vic@demuzere.be>
2024-01-13 13:47:15 +0100

    Add support for a subset of SVG elements
    This enables us to embed SVG graphics in HTML documents.

--- hypertext/tests/main.rs
+++ hypertext/tests/main.rs
@@ -40,3 +40,34 @@ fn readme() {
 
     assert_eq!(shopping_list_maud, shopping_list_rsx);
 }
+
+#[test]
+#[cfg(feature = "svg")]
+fn inline_svg() {
+    use hypertext::{html_elements, GlobalAttributes, GlobalSVGAttributes, PresentationSVGAttributes, Renderable};
+    
+    // Icon from Bootstrap Icons
+    // https://icons.getbootstrap.com/icons/chat-dots/
+
+    let content_maud = hypertext::maud!{
+        div #test {
+            svg .bi."bi-chat-dots" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16" {
+                path d="M5 8a1 1 0 1 1-2 0 1 1 0 0 1 2 0m4 0a1 1 0 1 1-2 0 1 1 0 0 1 2 0m3 1a1 1 0 1 0 0-2 1 1 0 0 0 0 2";
+                path d="m2.165 15.803.02-.004c1.83-.363 2.948-.842 3.468-1.105A9 9 0 0 0 8 15c4.418 0 8-3.134 8-7s-3.582-7-8-7-8 3.134-8 7c0 1.76.743 3.37 1.97 4.6a10.4 10.4 0 0 1-.524 2.318l-.003.011a11 11 0 0 1-.244.637c-.079.186.074.394.273.362a22 22 0 0 0 .693-.125m.8-3.108a1 1 0 0 0-.287-.801C1.618 10.83 1 9.468 1 8c0-3.192 3.004-6 7-6s7 2.808 7 6-3.004 6-7 6a8 8 0 0 1-2.088-.272 1 1 0 0 0-.711.074c-.387.196-1.24.57-2.634.893a11 11 0 0 0 .398-2";
+            }
+        }
+    }
+    .render();
+
+    let content_rsx = hypertext::rsx!{
+        <div id="test">
+            <svg class="bi bi-chat-dots" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
+                <path d="M5 8a1 1 0 1 1-2 0 1 1 0 0 1 2 0m4 0a1 1 0 1 1-2 0 1 1 0 0 1 2 0m3 1a1 1 0 1 0 0-2 1 1 0 0 0 0 2"/>
+                <path d="m2.165 15.803.02-.004c1.83-.363 2.948-.842 3.468-1.105A9 9 0 0 0 8 15c4.418 0 8-3.134 8-7s-3.582-7-8-7-8 3.134-8 7c0 1.76.743 3.37 1.97 4.6a10.4 10.4 0 0 1-.524 2.318l-.003.011a11 11 0 0 1-.244.637c-.079.186.074.394.273.362a22 22 0 0 0 .693-.125m.8-3.108a1 1 0 0 0-.287-.801C1.618 10.83 1 9.468 1 8c0-3.192 3.004-6 7-6s7 2.808 7 6-3.004 6-7 6a8 8 0 0 1-2.088-.272 1 1 0 0 0-.711.074c-.387.196-1.24.57-2.634.893a11 11 0 0 0 .398-2"/>
+            </svg>
+        </div>
+    }
+    .render();
+
+    assert_eq!(content_maud, content_rsx);
+}
