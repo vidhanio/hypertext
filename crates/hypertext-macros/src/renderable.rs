72bdfc03e149691e3d6c05e3afaf8014469fe878
Vidhan Bhatt <me@vidhan.io>
2026-07-18 10:15:00 -0400

    fix: use reexported bon in components

--- crates/hypertext-macros/src/renderable.rs
+++ crates/hypertext-macros/src/renderable.rs
@@ -148,6 +148,7 @@ pub fn generate(args: RenderableArgs, mut fn_item: ItemFn) -> syn::Result<TokenS
         .map(|attr| quote!(#attr))
         .collect::<Vec<_>>();
 
+    let uses_default_builder = args.builder.is_none() && !fields.is_empty();
     let builder = args.builder.or_else(|| {
         if fields.is_empty() {
             None
@@ -157,9 +158,14 @@ pub fn generate(args: RenderableArgs, mut fn_item: ItemFn) -> syn::Result<TokenS
     });
 
     if let Some(BuilderArg::Path(path)) = builder {
-        struct_attrs.push(quote! {
-            #[derive(#path)]
-        });
+        let bon_crate = uses_default_builder.then(|| quote!(#[builder(crate = ::hypertext::bon)]));
+        struct_attrs.insert(
+            0,
+            quote! {
+                #[derive(#path)]
+                #bon_crate
+            },
+        );
     }
 
     let fn_name = &fn_item.sig.ident;
